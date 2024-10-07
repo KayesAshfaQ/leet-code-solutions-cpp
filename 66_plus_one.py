3e7261c60aa734462c8c786f3f0ed67472570c5c
kayesAshfaq <asfak7074@gmail.com>
2024-10-07 10:36:20 +0600

    add solution for plus one problem in Python

---
+++ 66_plus_one.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = 0
+        for digit in digits:
+            num = num * 10 + digit
+
+        num += 1
+        digits.clear()
+        while num > 0:
+            digits.insert(0, num % 10)
+            num //= 10
+
+        return digits
